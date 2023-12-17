2b189055b46c0276020598ed11946bd1d9ab293c
MehracAli <mehrajvm@code.edu.az>
2023-12-17 20:17:24 +0400

    hw done

---
+++ DovrForma2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+string star = "*";
+string tre = new String('-', 30);
+
+char[] stars = ['*'];
+
+for (int i = 1; i < 6; i++)
+{
+    Array.Resize(ref stars, 1 + i);
+    stars[i] = '*';
+}
+
+var str = new StringBuilder();
+
+for (int i = 0; i < stars.Length; i++)
+{
+    str.Append(stars[i]);
+}
+
+Console.WriteLine(tre);
+//Task 1//
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(str.ToString().Substring(0, i));
+}
+
+
+Console.WriteLine(tre);
+//Task 2//
+
+char[] chars1 = str.ToString().ToCharArray();
+
+for (int i = 0; i < chars1.Length; i++)
+{
+    if (i == 0)
+    {
+        Console.WriteLine(string.Join(' ', chars1));
+    }
+    if (i > 1 && i< chars1.Length)
+    {
+        string st = string.Join(' ', chars1);
+        st = st.Replace('*', ' ');
+        st = st.Insert(0, "*").Insert(st.Length-1, "*");
+        Console.WriteLine(st);
+    }
+    if(i == chars1.Length-1)
+    {
+        Console.WriteLine(string.Join(' ', chars1));
+    }
+}
+
+
+
+
+Console.WriteLine(tre);
+//Task 3//
+char[] chars2 = str.ToString().ToCharArray();
+
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(string.Join(" ", chars2));
+}
---
+++ SendMail/Program.cs
@@ -0,0 +1,86 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+List<string> users = new List<string>();
+
+users.Add("Mehraj Mammadov");
+users.Add("Tajir Mammadov");
+users.Add("Vahid Mammadov");
+users.Add("Murat Vuranok");
+users.Add("Asger Hoca");
+
+Random rnd = new();
+int userOrder = rnd.Next(users.Count);
+
+string user = string.Empty;
+
+for (int i = -1; i < userOrder; i++)
+{
+    user = users[userOrder];
+}
+
+string[] userInfo = user.Split(" ");
+
+StringBuilder html = new($"""
+    <!DOCTYPE html>
+    <html lang = "en">
+
+    <head>
+      <meta charset = "UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title> Document </title>
+    </head>
+
+    <body style = "width: 30%; margin: 0 auto;">
+      <section class= "w-5">
+        <div style = "background: black;">
+          <div style = "color: white; text-align: center;">
+            <p> Name: {userInfo[0]} </p>
+            <p> Surname: {userInfo[1]} </p>
+          </div>
+        </div>
+      </section>
+    </body>
+
+    </html>
+""");
+
+
+Console.WriteLine(html);
+AlternateView plainView = AlternateView.CreateAlternateViewFromString($"Fullname: {userInfo[0]} {userInfo[1]}", null, "text/plain");
+MailMessage mailMessage = new MailMessage(
+                                        new MailAddress("mehrajvm@code.edu.az", $"{userInfo[0]} {userInfo[1]}"),
+                                        new MailAddress("isim@soyisim.com")
+                                        );
+mailMessage.Subject = "Tapsiriq mesaji";
+mailMessage.Body = html.ToString();
+mailMessage.IsBodyHtml = true;
+
+SmtpClient smtpClient = new SmtpClient();
+smtpClient.Host = "smtp.gmail.com";
+smtpClient.Port = 587;
+smtpClient.EnableSsl = true;
+smtpClient.UseDefaultCredentials = false;
+smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+smtpClient.Credentials = new NetworkCredential("mehrajvm@code.edu.az", "iscxkwonfrjjgegk");
+//smtpClient.Send(mailMessage);
+
+
+static async Task Execute()
+{
+    var apiKey = "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";
+    var client = new SendGridClient(apiKey);
+    var from = new EmailAddress("mehrajvm@code.edu.az", "Mehraj Mammadov");
+    var subject = "Sending with SendGrid is Fun";
+    var to = new EmailAddress("mehrajalimahmut@gmail.com", "Example User");
+    var plainTextContent = "and easy to do anywhere, even with C#";
+    var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+    var response = await client.SendEmailAsync(msg);
+    Console.WriteLine(response.StatusCode);
+    Console.WriteLine(response.Body.ReadAsStringAsync());
+}
+await Execute();
